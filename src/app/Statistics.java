d47914a931a4064ebf436ef44b29ea8b068c17c9
Wiktor <wiko700@gmail.com>
2023-09-19 16:45:32 +0200

    Optimized code and added getMostPopularAuthorPhysicalBooks() method

--- src/app/Statistics.java
+++ src/app/Statistics.java
@@ -4,40 +4,72 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Statistics {
+    private static List<List<Object>> listOfPhysicalBooks = PhysicalBook.getListOfAllBooks();
+    private static List<List<Object>> listOfAudioBooks = AudioBook.getListOfAllBooks();
+    private static List<List<Object>> listOfAllBooksBothPhysicalAndAudio = createListOfAllBooks(
+            listOfPhysicalBooks, listOfAudioBooks
+    );
+
     public static List<String> getMostPopularAuthor() {
+        Set<String> allAuthors = listOfAllBooksBothPhysicalAndAudio
+                .stream()
+                .map(list -> Objects.toString(list.get(1)))
+                .collect(Collectors.toSet());
+        Map<String, Integer> authorsAndNumberOfTheirBooksInLibrary = createMapOfAuthorsAndNumberOfTheirBooksInLibrary(
+                listOfAllBooksBothPhysicalAndAudio, allAuthors
+        );
+        List<String> mostPopularAuthors = createListOfMostPopularAuthors(authorsAndNumberOfTheirBooksInLibrary);
+        return mostPopularAuthors;
+    }
+    public static List<String> getMostPopularAuthorPhysicalBooks() {
+        Set<String> physicalBooksAuthors = listOfPhysicalBooks
+                .stream()
+                .map(list -> Objects.toString(list.get(1)))
+                .collect(Collectors.toSet());
+        Map<String, Integer> authorsAndNumberOfTheirBooksInLibrary = createMapOfAuthorsAndNumberOfTheirBooksInLibrary(
+                listOfPhysicalBooks, physicalBooksAuthors
+        );
+        List<String> mostPopularAuthorsOfPhysicalBooks = createListOfMostPopularAuthors(authorsAndNumberOfTheirBooksInLibrary);
+        return mostPopularAuthorsOfPhysicalBooks;
+    }
+
+//    public static String getMostPopularAuthorAudioBooks();
+//    public static String getMostPopularPublicationDate();
+//    public static String getMostPopularPublicationDatePhysicalBooks();
+//    public static String getMostPopularPublicationDateAudioBooks();
+
+    private static List<List<Object>> createListOfAllBooks(
+            List<List<Object>> listOfPhysicalBooks, List<List<Object>> listOfAudioBooks
+    ) {
         List<List<Object>> listOfAllBooksBothPhysicalAndAudio = new ArrayList<>();
-        listOfAllBooksBothPhysicalAndAudio.addAll(PhysicalBook.getListOfAllBooks());
-        listOfAllBooksBothPhysicalAndAudio.addAll(AudioBook.getListOfAllBooks());
-        Set<String> allAuthors = listOfAllBooksBothPhysicalAndAudio.stream().map(list -> Objects.toString(list.get(1))).collect(Collectors.toSet());
-
+        listOfAllBooksBothPhysicalAndAudio.addAll(listOfPhysicalBooks);
+        listOfAllBooksBothPhysicalAndAudio.addAll(listOfAudioBooks);
+        return listOfAllBooksBothPhysicalAndAudio;
+    }
+    private static Map<String, Integer> createMapOfAuthorsAndNumberOfTheirBooksInLibrary(
+            List<List<Object>> listOfBooks, Set<String> listOfAuthors
+    ) {
         Map<String, Integer> authorsAndNumberOfTheirBooksInLibrary = new HashMap<>();
-
-        for (String author: allAuthors) {
+        for (String author: listOfAuthors) {
             int booksOfThisAuthor = 0;
-            for (List<Object> list: listOfAllBooksBothPhysicalAndAudio) {
+            for (List<Object> list: listOfBooks) {
                 if (list.contains(author)) {
                     booksOfThisAuthor++;
                 }
             }
             authorsAndNumberOfTheirBooksInLibrary.put(author, Integer.valueOf(booksOfThisAuthor));
         }
-
+        return authorsAndNumberOfTheirBooksInLibrary;
+    }
+    private static List<String> createListOfMostPopularAuthors(Map<String, Integer> authorsAndNumberOfTheirBooksInLibrary) {
         List<String> mostPopularAuthors = new ArrayList<>();
-
         for (String author: authorsAndNumberOfTheirBooksInLibrary.keySet()) {
             if (authorsAndNumberOfTheirBooksInLibrary.get(author) == Collections.max(
                     authorsAndNumberOfTheirBooksInLibrary.values()
             )) {
                 mostPopularAuthors.add(author);
             }
-
         }
-
         return mostPopularAuthors;
     }
-//    public static String getMostPopularAuthorPhysicalBooks();
-//    public static String getMostPopularAuthorAudioBooks();
-//    public static String getMostPopularPublicationDate();
-//    public static String getMostPopularPublicationDatePhysicalBooks();
-//    public static String getMostPopularPublicationDateAudioBooks();
 }
